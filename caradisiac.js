2d8aee168a4db549fa275bb2a792aa9500173c72
Survolt09 <cyril.caron95@gmail.com>
2018-03-22 23:35:31 +0100

    feat(models) get all the models

--- caradisiac.js
+++ caradisiac.js
@@ -1,7 +1,7 @@
 const {getBrands} = require('node-car-api');
 const {getModels} = require('node-car-api');
 const elasticsearch = require ('elasticsearch')
-
+  var fs = require('fs');
 
 /*async function print () {
   const brands = await getBrands();
@@ -22,18 +22,69 @@ async function print2 () {
 
 print2();*/
 
-var allMyModels=[]
 
-async function  getAllBrands(){
-  const brands = await getBrands();
+
+async function getAllBrands(){
+  var brands = await getBrands();
+  console.log("le nombre de marques est : " + brands.length)
   return brands;
 }
 
-getAllBrands().then(res => res.forEach( async brand =>{
-  var models = await getModels(brand);
-  console.log(models.length)
-  models.forEach( model=>{
-    allMyModels.push(model)
-    console.log(allMyModels.length)
+async function getAllModels(){
+  var allModel =[];
+  var brands = await getBrands();
+  var myPromises = brands.map((brand) =>getModels(brand))
+
+  await Promise.all(myPromises)
+    .then((result)=>{
+    result.forEach((Arraymodel)=>{
+      Arraymodel.forEach((model) =>{
+        allModel.push(model)
+      })
+    })
+    console.log(allModel);
+    indexDB(allModel)
   })
-}))
+  .catch((err) => console.log(err))
+}
+
+
+
+
+
+function indexDB(allModel){
+
+      var models;
+      allModel.forEach((elem) =>{
+        models += `{"index" : {"_index": "restaurants","_type" : "restaurant","_id" : "${elem.uuid}" }}` + "\n"
+        models += JSON.stringify(elem) + "\n"
+      })
+
+  fs.writeFile("models.json" , models ,'utf8' , (err)=>{
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log("Succefully wrote the file")
+    }
+  })
+}
+
+getAllModels();
+
+
+
+
+
+//createJSON();
+
+
+async function printModels(){
+   let brands = await getAllBrands();
+   let models = getAllModels(brands);
+   return models
+}
+/*
+printModels().then((models)=>{
+  console.log(models)
+})*/
